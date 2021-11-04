8ec91239296f7549c572af5c0f5a82935d39e2ab
rene rodriguez <reneclavijo@live.com>
2021-11-04 13:00:33 -0500

    Agregando la lógica para actualizar el hotel

--- app/controllers/hoteles_controller.rb
+++ app/controllers/hoteles_controller.rb
@@ -1,6 +1,6 @@
 class HotelesController < ApplicationController
 
-    before_action :asignar_hotel, only: [:editar]
+    before_action :asignar_hotel, only: [:editar, :actualizar, :eliminar]
 
     # GET /hoteles
     def listar
@@ -24,11 +24,30 @@ class HotelesController < ApplicationController
     def guardar
         @hotel = Hotel.new(params_hotel)
         if @hotel.save
-            # redirect_to hoteles_path # listar hoteles
+            redirect_to hoteles_path # listar hoteles
         else
             @ciudades = Ciudad.all
             render :nuevo
         end
+    end
+
+    # PATCH /hoteles/:id
+    def actualizar
+        @hotel.nombre = params_hotel[:nombre]
+        @hotel.estrellas = params_hotel[:estrellas]
+        @hotel.ciudad_id = params_hotel[:ciudad_id]
+        if @hotel.save
+            redirect_to hoteles_path
+        else
+            @ciudades = Ciudad.all
+            @texto = "Actualizar hotel desde actualizar"
+            render :editar
+        end
+    end
+
+    def eliminar
+        @hotel.destroy
+        redirect_to hoteles_path
     end
 
     private
