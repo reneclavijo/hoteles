863e17af2440e21e7f2c53d435ab6aff69bbe627
rene rodriguez <reneclavijo@live.com>
2021-11-11 12:56:05 -0500

    agregando la creación de la reserva para un usuario en particular

--- app/controllers/reservas_controller.rb
+++ app/controllers/reservas_controller.rb
@@ -11,6 +11,24 @@ class ReservasController < ApplicationController
 
     # POST
     def guardar
+        @habitacion = Habitacion.find(params[:id_habitacion])
+        @usuario    = Usuario.find(cookies[:usuario_id])
+        @reserva = Reserva.new(params_reserva)
+
+        @reserva.habitacion = @habitacion
+        @reserva.usuario    = @usuario
+        @reserva.codigo         = SecureRandom.alphanumeric(5)
+        if @reserva.save
+            flash[:reserva_creada] = "Su reserva ha sido creada"
+            redirect_to root_path
+        else
+            render :nueva
+        end
+    end
+
+    private
+    def params_reserva
+        return params.require(:reserva).permit(:fecha_ingreso, :fecha_salida)
     end
 
 end
