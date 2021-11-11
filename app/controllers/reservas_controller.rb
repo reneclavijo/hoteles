13f4ddbd158aa1e528761a4ae766904d0f9d876d
rene rodriguez <reneclavijo@live.com>
2021-11-11 11:15:56 -0500

    agregando la vista del formulario de reversas

--- app/controllers/reservas_controller.rb
+++ app/controllers/reservas_controller.rb
@@ -2,9 +2,13 @@ class ReservasController < ApplicationController
 
     layout 'layout_cliente'
     
+    # GET /reservas/:id_habitacion
     def nueva
+        @habitacion = Habitacion.find(params[:id_habitacion])
+        @usuario    = Usuario.find(cookies[:usuario_id])
     end
 
+    # POST
     def guardar
     end
 
